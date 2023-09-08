726585104366bbc99290778d219bacd148ac1134
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-09-08 22:07:51 +0300

    added video section and podcast section

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ let newsWrap = createNewsWrap();
 let newsWrapSmall = createNewsWrapSmall();
 let newsCardLarge = createNewsCardLarge();
 let newsItemSmall = createNewsItemSmall();
+let btnToAll = createBtnToAll();
+let videosSection = createVideosSection();
+let allVideos = createAllVideos();
+let video = createVideo();
+let sectionSubtitle = createSectionSubtitle();
+let rightSide = createRightSide();
+let podcastsSection = createPodcastsSection();
+let podcastsWrap = createPodcastsWrap();
+let podcastContainer = createPodcastContainer();
 
-container.append(leftSide);
-leftSide.append(newsSection);
+container.append(leftSide, rightSide);
+leftSide.append(newsSection, videosSection);
 
 function createLeftSide() {
   let leftSide = document.createElement("div");
@@ -37,7 +46,9 @@ function createNewsItems() {
   return newsItems;
 }
 
-newsSection.append(sectionTitleNews, newsItems);
+let btnToAllNews = createBtnToAll("Visos naujienos");
+
+newsSection.append(sectionTitleNews, newsItems, btnToAllNews);
 
 function createNewsWrap() {
   let newsWrap = document.createElement("div");
@@ -178,4 +189,154 @@ newsWrapSmall.append(
   newsItemSmall2,
   newsItemSmall3,
   newsItemSmall4
+);
+
+function createBtnToAll(title) {
+  let btnToAll = document.createElement("a");
+  btnToAll.classList.add("btn-to-all");
+  btnToAll.textContent = title;
+  return btnToAll;
+}
+
+function createVideosSection() {
+  let videosSection = document.createElement("section");
+  videosSection.classList.add("videos");
+  return videosSection;
+}
+
+let sectionTitleVideos = createSectionTitle("Vaizdo įrašai");
+
+function createAllVideos() {
+  let allVideos = document.createElement("div");
+  allVideos.classList.add("all-videos");
+  return allVideos;
+}
+
+function createSectionSubtitle(title) {
+  let sectionSubtitle = document.createElement("h3");
+  sectionSubtitle.textContent = title;
+  return sectionSubtitle;
+}
+
+let sectionSubtitleVideos = createSectionSubtitle("Jei galiu aš - gali ir tu!");
+let btnToAllVideos = createBtnToAll("Visi vaizdo įrašai");
+videosSection.append(
+  sectionTitleVideos,
+  allVideos,
+  sectionSubtitleVideos,
+  btnToAllVideos
+);
+
+function createVideo() {
+  let video = document.createElement("div");
+  video.classList.add("video");
+  video.innerHTML = `<iframe width="560" height="415" src="https://www.youtube.com/embed/b8dGCsP75HA"
+                                title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>`;
+  return video;
+}
+
+allVideos.append(video);
+
+function createRightSide() {
+  let rightSide = document.createElement("div");
+  rightSide.classList.add("right-side");
+  return rightSide;
+}
+
+function createPodcastsSection() {
+  let podcastsSection = document.createElement("section");
+  podcastsSection.classList.add("podcasts");
+  return podcastsSection;
+}
+
+let sectionTitlePodcasts = createSectionTitle("Podcastai ir radijo laidos");
+
+function createPodcastsWrap() {
+  let podcastsWrap = document.createElement("div");
+  podcastsWrap.classList.add("podcasts-wrap");
+  return podcastsWrap;
+}
+
+rightSide.append(podcastsSection, sectionTitlePodcasts, podcastsWrap);
+
+function createPodcastContainer(imgSrc, playTime, title, date) {
+  let podcastContainer = document.createElement("div");
+  podcastContainer.classList.add("podcast-container");
+
+  let podcastCardTop = document.createElement("div");
+  podcastCardTop.classList.add("podcast-card-top");
+
+  let podcastImg = document.createElement("img");
+  podcastImg.src = imgSrc;
+  podcastImg.style.width = "70px";
+  podcastImg.style.height = "70px";
+
+  let playTimeDiv = document.createElement("div");
+  playTimeDiv.classList.add("play-time");
+  playTimeDiv.textContent = `Trukmė: ${playTime}`;
+
+  let playButton = document.createElement("button");
+  playButton.classList.add("play-btn");
+
+  let playImg = document.createElement("img");
+  playImg.src = "./images/play-icon.webp";
+  playImg.style.width = "20px";
+  playImg.style.height = "20px";
+
+  playButton.append(playImg);
+
+  podcastCardTop.append(podcastImg, playTimeDiv, playButton);
+
+  let podcastCardContent = document.createElement("div");
+  podcastCardContent.classList.add("podcast-card-content");
+
+  let podcastTitle = document.createElement("h3");
+  podcastTitle.textContent = title;
+
+  let podcastDate = document.createElement("span");
+  podcastDate.classList.add("news-upload-date");
+  podcastDate.textContent = date;
+
+  podcastCardContent.append(podcastTitle, podcastDate);
+
+  podcastContainer.append(podcastCardTop, podcastCardContent);
+
+  return podcastContainer;
+}
+
+let podcastContainer1 = createPodcastContainer(
+  `./images/Untitled-design-260x260.png`,
+  `7:55`,
+  `Pojūčius pirštų galiukams sugrąžinti gali smegenų implantas`,
+  `2021-09-02`
+);
+
+let podcastContainer2 = createPodcastContainer(
+  `./images/Untitled-design-260x260.png`,
+  `7:54`,
+  `Elektrinių povandeninių dronų kompanija pasirengusi tapti vandenynų „SpaceX“`,
+  `2021-08-25`
+);
+
+let podcastContainer3 = createPodcastContainer(
+  `./images/Untitled-design-260x260.png`,
+  `9:22`,
+  `Kaip gyvensime 2031-aisiais? 10 esminių prognozių`,
+  `2021-08-18`
+);
+
+let podcastContainer4 = createPodcastContainer(
+  `./images/Untitled-design-260x260.png`,
+  `8:32`,
+  `Jei virtualus pasaulis taps realybe, jos neturėtų valdyti „Facebook“`,
+  `2021-08-11`
+);
+
+podcastsWrap.append(
+  podcastContainer1,
+  podcastContainer2,
+  podcastContainer3,
+  podcastContainer4
 );
